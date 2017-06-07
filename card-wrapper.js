2632261de35db1413b9f28998d35c4b197918529
Michael Vitale <mmvitale@gmail.com>
2017-06-07 11:58:16 -0400

    move more functionality into card-wrapper

--- card-wrapper.js
+++ card-wrapper.js
@@ -6,11 +6,17 @@
 var exports = (function() {
   var exports = {};
 
-  var templateSupplier = null;
+  var templateSupplier = null
+    , renderer = null
+    ;
 
   exports.setTemplateSupplier = function(supplier) {
     templateSupplier = supplier;
   };
+
+  exports.setRenderer = function(theRenderer) {
+    renderer = theRenderer
+  }
 
   exports.newInstance = function(card, cb) {
     if (!templateSupplier) {
@@ -22,13 +28,21 @@ var exports = (function() {
         return cb(err);
       }
 
-      return cb(null, new CardWrapper(card, template));
+      // TODO: refactor
+      renderer.setCard(card, function(err) {
+        if (err) {
+          return cb(err);
+        }
+
+        return cb(null, new CardWrapper(card, template, renderer));
+      });
     });
   };
 
-  function CardWrapper(card, template) {
+  function CardWrapper(card, template, renderer) {
     var card = card
       , template = template
+      , renderer = renderer
       ;
 
     var defaultZoomLevel = 0
@@ -96,6 +110,104 @@ var exports = (function() {
         width: field.width,
         height: field.height
       };
+    }
+
+    function fieldForId(id) {
+      var field = template.fields[id];
+      return Object.assign({ id: id}, field);
+    }
+
+    /*
+     * Get all editable fields from the Card's template
+     */
+    function editableFields() {
+      return fields().filter(function(field) {
+        return field.label != null;
+      });
+    }
+
+    /*
+     * Get all fields from the Card's template
+     */
+    function fields() {
+      var ret = []
+        , fieldIds = Object.keys(template.fields)
+        , fieldId = null
+        , field = null;
+
+      for (var i = 0; i < fieldIds.length; i++) {
+        fieldId = fieldIds[i];
+
+        ret.push(fieldForId(fieldId));
+      }
+
+      return ret;
+    }
+
+    function imageFields() {
+      return editableFields().filter((field) => {
+        return field.type === 'image';
+      });
+    }
+    this.imageFields = imageFields;
+
+    function resolveChoice(choiceIndex, fieldChoices) {
+      var chosenValue = null;
+
+      if (typeof choiceIndex === 'number') {
+        chosenValue = fieldChoices[choiceIndex];
+      } else if (Array.isArray(choiceIndex)) { // Assume array of indices
+        chosenValue = [];
+
+        choiceIndex.forEach(function(index) {
+         chosenValue.push(fieldChoices[index]);
+        });
+      }
+
+      return chosenValue;
+    }
+
+    this.getFieldValue = function(fieldId) {
+      var field = fieldForId(fieldId)
+        , fieldValue = field.value
+        , fieldChoices = card.choices[field.id]
+        , dataValue = card.data[field.id]
+        , dataSrc = null
+        , chosenValue = null
+        ;
+
+      /*
+       * Choose data from, in order of preference:
+       * 1) User-supplied data
+       * 2) Card default data
+       * 3) Field default data
+       */
+      // != null is true for undefined as well (don't use !==)
+      if (dataValue != null) {
+        dataSrc = dataValue;
+      } else if (fieldValue != null) {
+        dataSrc = field;
+      }
+
+      if (dataSrc != null) {
+        if (dataSrc.choiceIndex != null) {
+          chosenValue = resolveChoice(dataSrc.choiceIndex, fieldChoices);
+
+          if (dataSrc.value != null) {
+            Object.assign(chosenValue, dataSrc.value);
+          }
+        } else {
+          chosenValue = dataSrc.value;
+        }
+      }
+
+      return chosenValue;
+    }
+
+    this.draw = function() {
+      renderer.draw(function(err) {
+        if (err) console.log(err);
+      });
     }
 
     this.rawCard = card;
