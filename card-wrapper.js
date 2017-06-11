7ecf137bfba1fb6eb8ff46798e82b0c6953a7d44
Michael Vitale <mmvitale@gmail.com>
2017-06-11 15:55:23 -0400

    move buildDrawingData to card-wrapper

--- card-wrapper.js
+++ card-wrapper.js
@@ -28,14 +28,7 @@ var exports = (function() {
         return cb(err);
       }
 
-      // TODO: refactor
-      renderer.setCard(card, function(err) {
-        if (err) {
-          return cb(err);
-        }
-
-        return cb(null, new CardWrapper(card, template, renderer));
-      });
+      return cb(null, new CardWrapper(card, template, renderer));
     });
   };
 
@@ -43,10 +36,21 @@ var exports = (function() {
     var card = card
       , template = template
       , renderer = renderer
+      , that = this
       ;
 
     var defaultZoomLevel = 0
       ;
+
+    function width() {
+      return template.width;
+    }
+    that.width = width;
+
+    function height() {
+      return template.height;
+    }
+    that.height = height;
 
     function checkFieldNameValid(name, type) {
       // TODO: check this condition
@@ -205,13 +209,344 @@ var exports = (function() {
     }
 
     this.draw = function() {
-      renderer.draw(function(err) {
+      renderer.draw(that, function(err) {
         if (err) console.log(err);
       });
     }
 
+    function resolveColor(colorSchemes, value) {
+      var schemeName = null
+        , schemeField = null
+        , parts = null
+        ;
+
+      if (value.startsWith('$')) {
+        parts = value.substring(1).split('.');
+        schemeName = parts[0];
+        schemeField = parts[1];
+
+        value = colorSchemes[schemeName][schemeField];
+      }
+
+      return value;
+    }
+
+    function buildColorData(field, data, colorSchemes) {
+      var resolvedColor = resolveColor(colorSchemes, data.color);
+
+      return {
+        type: 'color',
+        x: field.x,
+        y: field.y,
+        height: field.height,
+        width: field.width,
+        color: resolvedColor
+      };
+    }
+
+    function buildLineData(field, colorSchemes) {
+      var resolvedColor = resolveColor(colorSchemes, field.color);
+
+      return {
+        type: 'line',
+        color: resolvedColor,
+        startX: field.startX,
+        startY: field.startY,
+        endX: field.endX,
+        endY: field.endY,
+        width: field.width
+      };
+    }
+
+    function buildTextData(field, data, colorSchemes) {
+      var text = data == null ? '' : data.text;
+
+      return buildTextDataHelper(
+        field.x,
+        field.y,
+        field.font,
+        field.color,
+        field.prefix,
+        field.wrapAt,
+        field.textAlign,
+        text,
+        colorSchemes
+      );
+    }
+
+    function buildKeyValTextData(field, data, colorSchemes) {
+      return [
+        buildTextDataHelper(
+          field.keyX,
+          field.y,
+          field.font,
+          field.color,
+          field.prefix,
+          field.wrapAt,
+          field.textAlign,
+          data.key.text,
+          colorSchemes
+        ),
+        buildTextDataHelper(
+          field.valX,
+          field.y,
+          field.font,
+          field.color,
+          field.prefix,
+          field.wrapAt,
+          field.textAlign,
+          data.val.text,
+          colorSchemes
+        )
+      ];
+    }
+
+    function buildTextDataHelper(
+      x,
+      y,
+      font,
+      color,
+      prefix,
+      wrapAt,
+      textAlign,
+      text,
+      colorSchemes
+    ) {
+      var resolvedColor = resolveColor(colorSchemes, color);
+
+      return {
+        type: 'text',
+        font: font,
+        color: resolvedColor,
+        text: text,
+        prefix: prefix,
+        x: x,
+        y: y,
+        wrapAt: wrapAt,
+        textAlign: textAlign
+      };
+    }
+
+    function buildImageData(field, data, colorSchemes) {
+      var results = [];
+
+      if (field.credit) {
+        results.push(buildTextData(field.credit, data.credit, colorSchemes));
+      }
+
+      results.push(buildImageDataHelper(field, data));
+
+      return results;
+    }
+
+    function buildImageDataHelper(field, data) {
+        var result = {
+          type: 'image',
+          x: field.x,
+          y: field.y,
+          height: field.height,
+          width: field.width,
+          panX: data.panX,
+          panY: data.panY,
+          rotate: data.rotate,
+          flipVert: data.flipVert,
+          flipHoriz: data.flipHoriz,
+          zoomLevel: data.zoomLevel,
+          url: data.url,
+          id: field.id
+        };
+
+      return result;
+    }
+
+    function buildMultiImageDataHelper(fields, datas, colorSchemes) {
+      var results = [];
+
+      for (var i = 0; i < fields.length; i++) {
+        var field = fields[i]
+          , data = datas[i]
+          ;
+
+        results.push(buildImageDataHelper(field, data));
+      }
+
+      return results;
+    }
+
+    function buildMultiImageData(field, data, colorSchemes) {
+      var results = []
+        , specs
+        ;
+
+      if (data.length) {
+        specs = field.specs[data.length - 1];
+        results = buildMultiImageDataHelper(
+          specs.slice(0),
+          data.slice(0),
+          colorSchemes
+        );
+      }
+
+      return results;
+    }
+
+    function buildKeyValListData(field, data, colorSchemes) {
+      var curData = null
+        , offsetField = null
+        , yOffset = 0
+        , results = []
+        ;
+
+      for (var i = 0; i < data.length; i++) {
+        // Build data for key-val element, setting the y value according
+        // to the field's yIncr and y values
+        curData = data[i];
+
+        yOffset = i * field.yIncr + field.y;
+        offsetField = Object.assign({}, field.keyValSpec);
+        offsetField.y += yOffset;
+
+        results = results.concat(
+          buildKeyValTextData(offsetField, curData, colorSchemes)
+        );
+
+        // additionalElements (which do not require data)
+        field.additionalElements.forEach(function(elemField) {
+          switch(elemField.type) {
+            case 'line':
+              offsetField = Object.assign({}, elemField);
+              offsetField.startY += yOffset;
+              offsetField.endY += yOffset;
+              results.push(buildLineData(offsetField, colorSchemes));
+              break;
+            default:
+              throw new Error('Unsupported field type: ' + elemField.type);
+          }
+        });
+      }
+
+      return results;
+    }
+
+    function buildDataForField(field, data, colorSchemes) {
+      var results;
+
+      switch (field.type) {
+        case 'color':
+          results = [buildColorData(field, data, colorSchemes)];
+          break;
+        case 'line':
+          results = [buildLineData(field, colorSchemes)];
+          break;
+        case 'text':
+          results = [buildTextData(field, data, colorSchemes)];
+          break;
+        case 'key-val-text':
+          results = buildKeyValTextData(field, data, colorSchemes);
+          break;
+        case 'image':
+        case 'labeled-choice-image':
+          results = buildImageData(field, data, colorSchemes);
+          break;
+        case 'multi-image':
+          results = buildMultiImageData(field, data, colorSchemes);
+          break;
+        case 'key-val-list':
+          results = buildKeyValListData(field, data, colorSchemes);
+          break;
+        default:
+          throw new Error('Invalid field type: ' + field.type);
+      }
+
+      return results;
+    }
+
+    function buildColorSchemes(colorSchemeFields) {
+      var colorSchemes = {};
+
+      colorSchemeFields.forEach(function(field) {
+        var colorScheme = resolveChosenValue(field);
+        colorSchemes[field.id] = colorScheme;
+      });
+
+      return colorSchemes;
+    }
+
+    function resolveChosenValue(field) {
+      var fieldValue = field.value
+        , fieldChoices = card.choices[field.id]
+        , dataValue = card.data[field.id]
+        , dataSrc = null
+        , chosenValue = null
+        ;
+
+      /*
+       * Choose data from, in order of preference:
+       * 1) User-supplied data
+       * 2) Card default data
+       * 3) Field default data
+       */
+      // != null is true for undefined as well (don't use !==)
+      if (dataValue != null) {
+        dataSrc = dataValue;
+      } else if (fieldValue != null) {
+        dataSrc = field;
+      }
+
+      if (dataSrc != null) {
+        if (dataSrc.choiceIndex != null) {
+          chosenValue = resolveChoice(dataSrc.choiceIndex, fieldChoices);
+
+          if (dataSrc.value != null) {
+            Object.assign(chosenValue, dataSrc.value);
+          }
+        } else {
+          chosenValue = dataSrc.value;
+        }
+      }
+
+      return chosenValue;
+    }
+
+    /*
+     * Build a list of elements renderable by the drawField method from the
+     * card's fields and data sources. Complex fields (e.g., key-val-text) are
+     * converted to primitive field types (e.g., text)
+     */
+    function buildDrawingData() {
+      var colorSchemeFields = []
+        , otherFields = []
+        , colorSchemes = null
+        , drawingData = []
+        ;
+
+      fields().forEach(function(field) {
+        if (field.type === 'color-scheme') {
+          colorSchemeFields.push(field);
+        } else {
+          otherFields.push(field);
+        }
+      });
+
+      colorSchemes = buildColorSchemes(colorSchemeFields);
+
+      otherFields.forEach(function(field) {
+        var chosenValue = resolveChosenValue(field)
+          , fieldDatas = buildDataForField(field, chosenValue, colorSchemes)
+
+        drawingData = drawingData.concat(fieldDatas);
+      });
+
+      return drawingData;
+    }
+    this.buildDrawingData = buildDrawingData;
+
     this.rawCard = card;
   }
+
+
+
+
 
   return exports;
 })();
--- template-renderer.js
+++ template-renderer.js
@@ -8,7 +8,6 @@
  * to draw the card to the Canvas instance.
  *
  * Parameters:
- *   templateSupplier - must implement templateSupplier.supply(templateName, cb),
  *   where templateName is the name of a card template, and the result is that
  *   template's 'spec' value.
  *
@@ -116,485 +115,42 @@
     }
   }
 
-  function TemplateRenderer(templateSupplier, canvasSupplier, imageFetcher) {
-    var card = null
-      , template = null
-      , canvas = null
-      , that = this
+  function TemplateRenderer(canvasSupplier, imageFetcher) {
+    var that = this
       , rotatedImageCache = new RotatedImageCache()
       ;
 
-    /*
-     * Set the Card to render. This method does not make a copy of the Card, so
-     * any external changes it will be reflected here. Calling this method causes
-     * the Card's template to be fetched, meaning that the get*Fields methods can
-     * be called after this method completes; it also causes the drawing Canvas
-     * to be set, which can be accessed by calling the getCanvas method.
-     *
-     * Parameters:
-     *   theCard - the Card
-     *   cb - function(err)
-     */
-    this.setCard = function setCard(theCard, cb) {
-      card = theCard;
+    function resolveImage(url, cb) {
+      return imageFetcher.fetch(url, cb);
+    }
 
-      if (!card.data) {
-        card.data = {};
+    function resolveImagesHelper(imageDatas, urlsToImages, cb) {
+      var imageData;
+
+      if (imageDatas.length === 0) {
+        return cb(null, urlsToImages);
       }
 
-      if (!template || card.templateName !== template.name) {
-        templateSupplier.supply(card.templateName, function(err, theTemplate) {
-          if (err) return cb(err);
-          template = theTemplate;
-          canvas = canvasSupplier.supply(template.width, template.height);
+      imageData = imageDatas.pop();
 
-          return cb();
-        });
-      } else {
-        return cb();
-      }
-    }
-
-    /*
-     * Get all fields with type 'image' from the Card's template
-     */
-    function imageFields() {
-      return editableFields().filter((field) => {
-        return field["type"] === "image";
-      });
-    }
-    this.imageFields = imageFields;
-
-    function fieldForId(id) {
-      var field = template.fields[id];
-      return Object.assign({ id: id }, field);
-    }
-
-    /*
-     * Get all fields from the Card's template
-     */
-    function fields() {
-      var ret = []
-        , fieldIds = Object.keys(template.fields)
-        , fieldId = null
-        , field = null;
-
-      for (var i = 0; i < fieldIds.length; i++) {
-        fieldId = fieldIds[i];
-
-        ret.push(fieldForId(fieldId));
-      }
-
-      return ret;
-    }
-    this.fields = fields;
-
-    /*
-     * Get all editable fields from the Card's template
-     */
-    function editableFields() {
-      return fields().filter(function(field) {
-        return field.label != null;
-      });
-    }
-    this.editableFields = editableFields;
-
-    /*
-     * Get the Canvas on which the draw method will render the Card
-     */
-    function getCanvas() {
-      return canvas;
-    }
-    this.getCanvas = getCanvas;
-
-    function getFieldValue(fieldId) {
-      return resolveChosenValue(fieldForId(fieldId));
-    }
-    this.getFieldValue = getFieldValue;
-
-    function setFieldAttr(fieldId, key, value) {
-      var data = card.data[fieldId];
-
-      if (!data) {
-        data = {};
-        card.data[fieldId] = data;
-      }
-
-      if (!data.value) {
-        data.value = {};
-      }
-
-      data.value[key] = value;
-    }
-    this.setFieldAttr = setFieldAttr;
-
-    function resolveImage(image, url, cb) {
-      if (!image) {
-        if (url) {
-          return imageFetcher.fetch(url, cb);
-        } else {
-          return cb(new Error('No url provided'));
-        }
-      }
-
-      return cb(null, image);
-    }
-
-    function resolveChoice(choiceIndex, fieldChoices) {
-      var chosenValue = null;
-
-      if (typeof choiceIndex === "number") {
-        chosenValue = fieldChoices[choiceIndex];
-      } else if (Array.isArray(choiceIndex)) { // Assume array of indices
-        chosenValue = [];
-
-        choiceIndex.forEach(function(index) {
-         chosenValue.push(fieldChoices[index]);
-        });
-      }
-
-      return chosenValue;
-    }
-
-    function resolveColor(colorSchemes, value) {
-      var schemeName = null
-        , schemeField = null
-        , parts = null
-        ;
-
-      if (value.startsWith('$')) {
-        parts = value.substring(1).split('.');
-        schemeName = parts[0];
-        schemeField = parts[1];
-
-        value = colorSchemes[schemeName][schemeField];
-      }
-
-      return value;
-    }
-
-    function buildColorData(field, data, colorSchemes) {
-      var resolvedColor = resolveColor(colorSchemes, data.color);
-
-      return {
-        type: 'color',
-        x: field.x,
-        y: field.y,
-        height: field.height,
-        width: field.width,
-        color: resolvedColor
-      };
-    }
-
-    function buildLineData(field, colorSchemes) {
-      var resolvedColor = resolveColor(colorSchemes, field.color);
-
-      return {
-        type: 'line',
-        color: resolvedColor,
-        startX: field.startX,
-        startY: field.startY,
-        endX: field.endX,
-        endY: field.endY,
-        width: field.width
-      };
-    }
-
-    function buildTextData(field, data, colorSchemes) {
-      var text = data == null ? '' : data.text;
-
-      return buildTextDataHelper(
-        field.x,
-        field.y,
-        field.font,
-        field.color,
-        field.prefix,
-        field.wrapAt,
-        field.textAlign,
-        text,
-        colorSchemes
-      );
-    }
-
-    function buildKeyValTextData(field, data, colorSchemes) {
-      return [
-        buildTextDataHelper(
-          field.keyX,
-          field.y,
-          field.font,
-          field.color,
-          field.prefix,
-          field.wrapAt,
-          field.textAlign,
-          data.key.text,
-          colorSchemes
-        ),
-        buildTextDataHelper(
-          field.valX,
-          field.y,
-          field.font,
-          field.color,
-          field.prefix,
-          field.wrapAt,
-          field.textAlign,
-          data.val.text,
-          colorSchemes
-        )
-      ];
-    }
-
-    function buildTextDataHelper(
-      x,
-      y,
-      font,
-      color,
-      prefix,
-      wrapAt,
-      textAlign,
-      text,
-      colorSchemes
-    ) {
-      var resolvedColor = resolveColor(colorSchemes, color);
-
-      return {
-        type: 'text',
-        font: font,
-        color: resolvedColor,
-        text: text,
-        prefix: prefix,
-        x: x,
-        y: y,
-        wrapAt: wrapAt,
-        textAlign: textAlign
-      };
-    }
-
-    function buildImageData(field, data, colorSchemes, cb) {
-      var results = [];
-
-      if (field.credit) {
-        results.push(
-          buildTextData(field.credit, data.credit, colorSchemes));
-      }
-
-      buildImageDataHelper(field, data, function(err, imageData) {
+      resolveImage(imageData.url, function(err, image) {
         if (err) return cb(err);
 
-        results.push(imageData);
-        cb(null, results);
+        urlsToImages[imageData.url] = image;
+        resolveImagesHelper(imageDatas, urlsToImages, cb);
       });
     }
 
-    function buildImageDataHelper(field, data, cb) {
-      resolveImage(data.image, data.url, function(err, image) {
-        if (err) return cb(err);
-
-        var result = {
-          type: 'image',
-          x: field.x,
-          y: field.y,
-          height: field.height,
-          width: field.width,
-          panX: data.panX,
-          panY: data.panY,
-          rotate: data.rotate,
-          flipVert: data.flipVert,
-          flipHoriz: data.flipHoriz,
-          zoomLevel: data.zoomLevel,
-          image: image,
-          id: field.id
-        };
-
-        cb(null, result);
-      });
-    }
-
-    function buildMultiImageDataHelper(
-      fields, datas, colorSchemes, resultsAccum, cb
-    ) {
-      if (fields.length === 0) {
-        return cb(null, resultsAccum);
-      }
-
-      var field = fields.pop()
-        , data = datas.pop()
-        ;
-
-      buildImageDataHelper(field, data, function(err, imageData) {
-        if (err) return cb(err);
-
-        resultsAccum.push(imageData);
-        buildMultiImageDataHelper(fields, datas, colorSchemes, resultsAccum, cb);
-      });
-    }
-
-    function buildMultiImageData(field, data, colorSchemes, cb) {
-      // TODO: Validate data length etc.
-
-      if (data.length) {
-        var specs = field.specs[data.length - 1];
-        buildMultiImageDataHelper(specs.slice(0), data.slice(0), colorSchemes, [], cb);
-      } else {
-        cb(null, []);
-      }
-    }
-
-    function buildKeyValListData(field, data, colorSchemes, cb) {
-      var curData = null
-        , offsetField = null
-        , yOffset = 0
-        , results = []
-        ;
-
-      for (var i = 0; i < data.length; i++) {
-        // Build data for key-val element, setting the y value according
-        // to the field's yIncr and y values
-        curData = data[i];
-
-        yOffset = i * field.yIncr + field.y;
-        offsetField = Object.assign({}, field.keyValSpec);
-        offsetField.y += yOffset;
-
-        results = results.concat(
-          buildKeyValTextData(offsetField, curData, colorSchemes)
-        );
-
-        // Draw additionalElements (which do not require data)
-        field.additionalElements.forEach(function(elemField) {
-          switch(elemField.type) {
-            case 'line':
-              offsetField = Object.assign({}, elemField);
-              offsetField.startY += yOffset;
-              offsetField.endY += yOffset;
-              results.push(buildLineData(offsetField, colorSchemes));
-              break;
-            default:
-              return cb(new Error('Unsupported field type: ' + elemField.type));
-          }
-        });
-      }
-
-      return cb(null, results);
-    }
-
-    function buildDataForField(field, data, colorSchemes, cb) {
-      switch (field.type) {
-        case 'color':
-          cb(null, [buildColorData(field, data, colorSchemes)]);
-          break;
-        case 'line':
-          cb(null, [buildLineData(field, colorSchemes)]);
-          break;
-        case 'text':
-          cb(null, [buildTextData(field, data, colorSchemes)]);
-          break;
-        case 'key-val-text':
-          cb(null, buildKeyValTextData(field, data, colorSchemes));
-          break;
-        case 'image':
-        case 'labeled-choice-image':
-          buildImageData(field, data, colorSchemes, cb);
-          break;
-        case 'multi-image':
-          buildMultiImageData(field, data, colorSchemes, cb);
-          break;
-        case 'key-val-list':
-          buildKeyValListData(field, data, colorSchemes, cb);
-          break;
-        default:
-          return cb(new Error('Invalid field type: ' + field.type));
-      }
-    }
-
-    function buildColorSchemes(colorSchemeFields) {
-      var colorSchemes = {};
-
-      colorSchemeFields.forEach(function(field) {
-        var colorScheme = resolveChosenValue(field);
-        colorSchemes[field.id] = colorScheme;
+    function resolveImages(drawingData, cb) {
+      var imageDatas = drawingData.filter(function(data) {
+        return data.type === 'image';
       });
 
-      return colorSchemes;
-    }
-
-    function resolveChosenValue(field) {
-      var fieldValue = field.value
-        , fieldChoices = card.choices[field.id]
-        , dataValue = card.data[field.id]
-        , dataSrc = null
-        , chosenValue = null
-        ;
-
-      /*
-       * Choose data from, in order of preference:
-       * 1) User-supplied data
-       * 2) Card default data
-       * 3) Field default data
-       */
-      // != null is true for undefined as well (don't use !==)
-      if (dataValue != null) {
-        dataSrc = dataValue;
-      } else if (fieldValue != null) {
-        dataSrc = field;
-      }
-
-      if (dataSrc != null) {
-        if (dataSrc.choiceIndex != null) {
-          chosenValue = resolveChoice(dataSrc.choiceIndex, fieldChoices);
-
-          if (dataSrc.value != null) {
-            Object.assign(chosenValue, dataSrc.value);
-          }
-        } else {
-          chosenValue = dataSrc.value;
-        }
-      }
-
-      return chosenValue;
-    }
-
-    function buildDrawingDataHelper(colorSchemes, fields, drawingData, cb) {
-      if (fields.length === 0) {
-        return cb(null, drawingData.reverse());
-      }
-
-      var field = fields.pop()
-        , chosenValue = resolveChosenValue(field)
-        ;
-
-      buildDataForField(field, chosenValue, colorSchemes, function(err, fieldResults) {
-        if (err) return cb(err);
-
-        drawingData = drawingData.concat(fieldResults);
-        return buildDrawingDataHelper(colorSchemes, fields, drawingData, cb);
-      });
+      return resolveImagesHelper(imageDatas, {}, cb);
     }
 
     /*
-     * Build a list of elements renderable by the drawField method from the
-     * card's fields and data sources. Complex fields (e.g., key-val-text) are
-     * converted to primitive field types (e.g., text)
-     */
-    function buildDrawingData(fields, cb) {
-      var colorSchemeFields = []
-        , otherFields = []
-        , colorSchemes = null
-        ;
-
-      fields.forEach(function(field) {
-        if (field.type === 'color-scheme') {
-          colorSchemeFields.push(field);
-        } else {
-          otherFields.push(field);
-        }
-      });
-
-      colorSchemes = buildColorSchemes(colorSchemeFields);
-      buildDrawingDataHelper(colorSchemes, otherFields, [], cb);
-    }
-
-    /*
-     * Render the Card on the Canvas
+     * Render a Card on the Canvas
      *
      * Parameters:
      *   cb - function(err, result)
@@ -602,26 +158,42 @@
      * Result:
      *   The Canvas with the Card rendered on it
      */
-    function draw(cb) {
-      buildDrawingData(fields(), function(err, drawingData) {
-        if (err) return cb(err);
+    function draw(card, cb) {
+      var canvas
+        , ctx
+        , drawingData
+        , imageDatas
+        , images = {}
+        ;
 
-        var ctx = canvas.getContext('2d');
+      try {
+        drawingData = card.buildDrawingData();
+      } catch (e) {
+        return cb(e);
+      }
 
-        drawingData.forEach(function(data) {
-          drawField(ctx, data);
-        });
+      // Resolve images up-front to prevent flickering due to loading delay in
+      // the middle of drawing to the canvas
+      resolveImages(drawingData, function(err, urlsToImages) {
+        // Moving this to just before drawing seems to prevent flickering on
+        // redraw
+        canvas = canvasSupplier.supply(card.width(), card.height());
+        ctx = canvas.getContext('2d');
+
+        try {
+          drawingData.forEach(function(data) {
+            drawField(ctx, data, urlsToImages);
+          });
+        } catch (e) {
+          return cb(e);
+        }
 
         return cb(null, canvas);
       });
     }
     this.draw = draw;
 
-    function fieldRequiresData(field) {
-      return field.type !== 'line';
-    }
-
-    function drawField(ctx, data) {
+    function drawField(ctx, data, urlsToImages) {
       switch(data.type) {
         case 'color':
           drawColor(ctx, data);
@@ -633,7 +205,7 @@
           drawText(ctx, data);
           break;
         case 'image':
-          drawImage(ctx, data);
+          drawImage(ctx, data, urlsToImages[data.url]);
           break;
         default:
           // TODO: Handle this case
@@ -712,23 +284,21 @@
       return parseFloat(fontArgs[0].replace('px', ''));
     }
 
-    function drawImage(ctx, data) {
+    function drawImage(ctx, data, image) {
       var targetRatio = (data.width * 1.0) / data.height
-        , imageHeight = typeof(data.image.naturalHeight) === "undefined" ?
-            data.image.height :
-            data.image.naturalHeight
-        , imageWidth = typeof(data.image.naturalWidth) === "undefined" ?
-            data.image.width :
-            data.image.naturalWidth
+        , imageHeight = typeof(image.naturalHeight) === "undefined" ?
+            image.height :
+            image.naturalHeight
+        , imageWidth = typeof(image.naturalWidth) === "undefined" ?
+            image.width :
+            image.naturalWidth
         , imageRatio = (imageWidth * 1.0) / imageHeight
         , sx = 0
         , sy = 0
         , sWidth = 0
         , sHeight = 0
         , gap = 0
-        , dx = -data.width / 2
-        , dy = -data.height / 2
-        , imageToDraw = data.image
+        , imageToDraw = image
         , rotateRad
         , invRotateRad
         , $tmpCanvas
@@ -763,13 +333,10 @@
         sy += data.panY * sHeight / 300;
       }
 
-      ctx.save();
-      ctx.translate(data.x + data.width / 2, data.y + data.height / 2);
-
       if (data.rotate || data.flipHoriz || data.flipVert) {
         imageToDraw =
           rotatedImageCache.get(
-            data.image,
+            image,
             data.id,
             data.rotate || 0,
             data.flipHoriz || false,
@@ -785,13 +352,11 @@
         sy,
         sWidth,
         sHeight,
-        dx,
-        dy,
+        data.x,
+        data.y,
         data.width,
         data.height
       );
-
-      ctx.restore();
     }
 
     function drawLine(ctx, data) {
